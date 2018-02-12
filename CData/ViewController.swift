f9c1278827eedb0880456e5ef5b19d0bcb136c78
GulMehru <gbmehru@gmail.com>
2018-02-12 22:36:59 +0500

    Initial Commit

---
+++ CData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CData
+//
+//  Created by Gul Mehru on 2/12/18.
+//  Copyright © 2018 Gul Mehru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
