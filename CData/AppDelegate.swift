1b654c12dde7d47d6ee6ff72be28147695e0e9b8
GulMehru <gbmehru@gmail.com>
2018-02-13 21:06:52 +0500

    data

--- CData/AppDelegate.swift
+++ CData/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
         return true
     }
@@ -90,4 +91,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
+let appdelegate = UIApplication.shared.delegate as! AppDelegate
+let context =  appdelegate.persistentContainer.viewContext
 
--- CData/ViewController.swift
+++ CData/ViewController.swift
@@ -7,19 +7,138 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    var data = [User]()
+    
+    let textViewName: UITextField = {
+        let text = UITextField()
+        text.frame = CGRect(x: 0, y: 0, width: 400, height: 50)
+        text.placeholder = "Enter your name"
+        text.backgroundColor = UIColor.lightGray
+        text.layer.masksToBounds = true
+        
+        text.textAlignment = .left
+        return text
+    }()
+    
+    let textViewClass: UITextField = {
+        let text = UITextField()
+        text.backgroundColor = UIColor.lightGray
+        text.frame = CGRect(x: 0, y: 0, width: 400, height: 50)
+        text.placeholder = "Enter your class"
+        text.layer.masksToBounds = true
+     
+        text.textAlignment = .left
+        return text
+    }()
+    
+    let enterButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 80, height: 40)
+        button.backgroundColor = UIColor.blue
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        return button
+    }()
+    
+    let showButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 80, height: 40)
+        button.backgroundColor = UIColor.blue
+        button.addTarget(self, action: #selector(showAction), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func buttonAction() {
+        insertData()
+    }
+    
+    @objc func showAction() {
+    
+    fetchData()
+    
+    }
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(textViewName)
+        view.addSubview(textViewClass)
+        view.addSubview(enterButton)
+        view.addSubview(showButton)
+        
+        view.addConstraintsWithFormat(format: "H:[v0(400)]", views: textViewName)
+        view.addConstraintsWithFormat(format: "V:|-20-[v0(35)]-20-[v1(35)]-20-[v2(50)]-20-[v3(50)]", views: textViewName, textViewClass, enterButton, showButton)
+        textViewName.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textViewClass.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        view.addConstraintsWithFormat(format: "H:[v0(80)]", views: enterButton)
+        enterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        view.addConstraintsWithFormat(format: "H:[v0(400)]", views: textViewClass)
+        //view.addConstraintsWithFormat(format: "H:|[v0]|", views: myTableView)
+        view.addConstraintsWithFormat(format: "H:[v0(80)]", views: showButton)
+        showButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+      
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func insertData() {
+        
+       
+        let entityDescription = NSEntityDescription.entity(forEntityName: "User", in: context)
+        
+        let user = User(entity: entityDescription!,
+                               insertInto: context)
+        
+        user.name = textViewName.text
+        user.abclass = textViewClass.text
+        
+        do{
+            try context.save()
+            textViewName.text = ""
+            textViewClass.text = ""
+            
+        }
+        catch let error {
+            //handle error
+        }
+        
+        
+        appdelegate.saveContext()
+        
+    }
+    
+    func fetchData() {
+        do {
+            data = try context.fetch(User.fetchRequest())
+            for each in data {
+                
+                print("Name: \(String(describing: each.name))\n")
+                print("Class: \(String(describing: each.abclass))\n")
+                
+            }
+            
+        }
+        
+        catch {
+            //handle error
+        }
     }
 
-
+    
 }
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        
+        var viewsDictionary = [String: UIView]()
+        for(index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
+
 
